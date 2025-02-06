8d3c866a1a60a3bffe06e2173c29e4a0ac230733
Noor ul Hasanat <noorulhasanat123@gmail.com>
2025-02-06 17:38:25 +0500

    The cde till here is working good now i am forwording towords git installiations

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import { Dummyrouting } from './component/Dummyrouting';
+// import MainRouter from './MComponents/MainRouter';
+// import Routing from './component/Routing';
+
+function App() {
+  return (
+    <>
+    <Dummyrouting/>
+    {/* <Routing/>
+   <MainRouter/> */}
+     </>
+  );
+}
+
+
+export default App;
